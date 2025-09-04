1153e4edc305ed962f4a3ef3fffdc1a5eb0402f0
Ruslan Somov <rusl.somow@gmail.com>
2025-09-04 17:28:16 +0300

    Task manager added

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,203 @@
+import netology.ru.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindByQueryInSimpleTask() {
+
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches("Позвонить родителям");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindByQueryInSimpleTask() {
+
+        SimpleTask simpleTask = new SimpleTask(1, "Постирать бельё");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+
+        boolean expected = false;
+        boolean actual = simpleTask.matches("Купить продукты");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindByQueryInEpic() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Todos todos = new Todos();
+
+        todos.add(epic);
+
+        boolean expected = true;
+        boolean actual = epic.matches("Молоко");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindByQueryInEpic() {
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Todos todos = new Todos();
+
+        todos.add(epic);
+
+        boolean expected = false;
+        boolean actual = epic.matches("Курица");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindByQueryInMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(meeting);
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Выкатка 3й версии приложения");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindByQueryInMeeting() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(meeting);
+
+        boolean expected = false;
+        boolean actual = meeting.matches("Приложение Нетология");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask};
+        Task[] actual = todos.search("Позвонить родителям");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInEpic() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Яйца");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTasksInMeeting() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("Приложение НетоБанка");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
